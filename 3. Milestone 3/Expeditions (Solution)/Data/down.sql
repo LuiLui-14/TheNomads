4205a88dbeaad520c3e11ba6a3bb0b274677cd72
ApneaMan29 <dmcbride15@wou.edu>
2021-01-29 13:39:15 -0800

    Added FK and fully implemented TeamMember/ExpeditionMap/Nation tables int he DB. Also stylized the Expeditions Home menu at the top of site so that it blended together.

--- 3. Milestone 3/Expeditions (Solution)/Data/down.sql
+++ 3. Milestone 3/Expeditions (Solution)/Data/down.sql
@@ -3,6 +3,9 @@
 ALTER TABLE [Expedition] DROP CONSTRAINT [Expedition_FK_Peak];
 ALTER TABLE [Expedition] DROP CONSTRAINT [Expedition_FK_TrekkingAgency];
 ALTER TABLE [TeamMember] DROP CONSTRAINT [TeamMember_FK_Nation];
+ALTER TABLE [ExpeditionMap] DROP CONSTRAINT [ExpeditionMap_FK_Expedition];
+ALTER TABLE [ExpeditionMap] DROP CONSTRAINT [ExpeditionMap_FK_TeamMember];
+ALTER TABLE [TeamMember] DROP CONSTRAINT [TeamMember_FK_Nation];
 
 DROP TABLE [Expedition];
 DROP TABLE [Peak];
@@ -10,6 +13,7 @@ DROP TABLE [TrekkingAgency];
 
 DROP TABLE [NewsArticle];
 
+DROP TABLE [ExpeditionMap];
 DROP TABLE [TeamMember];
 DROP TABLE [Nation];
 
