ab22033c1dc18fca011a1dc1373ac3449f668e3d
ApneaMan29 <dmcbride15@wou.edu>
2021-02-18 10:23:08 -0800

    Revert "Reverted previous DefaultConnection and moved the placeholder to AltDefaultConnection"
    This reverts commit ab020f0eb5c9e258711a0c08346a97708819a152.

--- TeamProject/Playlistofy/Startup.cs
+++ TeamProject/Playlistofy/Startup.cs
@@ -20,6 +20,7 @@ namespace Playlistofy
     {
         private string _spotifyClientId = null;
         private string _spotifyClientSecret = null;
+        private string _connectionSecret = null;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
