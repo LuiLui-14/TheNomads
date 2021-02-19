3266ce6dc8a582c7904816a6fad5f7ce294c7efc
Luis Olvera <lolvera18@mail.wou.edu>
2021-02-18 22:46:07 -0800

    Feature complete - Am able to user's playlists from spotify

---
+++ TeamProject/Playlistofy/Models/Playlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using SpotifyAPI.Web;
+
+namespace Playlistofy.Models
+{
+    public class Playlist
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool? Collaborative { get; set; }
+        public bool? Public { get; set; }
+        public string? Id { get; set; } = default!;
+        public string? Href { get; set; } = default!;
+        public string? SnapshotId { get; set; } = default!;
+        public List<Image>? Images { get; set; } = default!;
+
+        public Paging<PlaylistTrack<IPlayableItem>>? Tracks { get; set; } = default!;
+        public string? Type { get; set; } = default!;
+        public string? Uri { get; set; } = default!;
+    }
+}
