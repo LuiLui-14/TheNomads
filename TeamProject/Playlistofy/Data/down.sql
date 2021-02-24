51760591fe8907a6f5bf420f0c54a774fae9b59f
Luis Olvera <lolvera18@mail.wou.edu>
2021-02-24 10:16:36 -0800

    Created scripts for new table

--- TeamProject/Playlistofy/Data/down.sql
+++ TeamProject/Playlistofy/Data/down.sql
@@ -1,3 +1,6 @@
 ﻿DROP Table [User];
+DROP Table [Playlist];
 
-ALTER TABLE [User] DROP CONSTRAINT [PK_User]; 
+ALTER TABLE [User] DROP CONSTRAINT [PK_User];
+ALTER TABLE [Playlist] DROP CONSTRAINT [PK_Playlist];
+ALTER TABLE [Playlist] DROP CONSTRAINT [Playlist_FK_USER];
--- TeamProject/Playlistofy/Models/Playlist.cs
+++ TeamProject/Playlistofy/Models/Playlist.cs
@@ -6,17 +6,12 @@ namespace Playlistofy.Models
 {
     public class Playlist
     {
-        public string? Name { get; set; }
+        public string Id { get; set; }
+        public string UserId { get; set; }
         public string? Description { get; set; }
-        public bool? Collaborative { get; set; }
+        public string Href { get; set; }
+        public string? Name { get; set; }
         public bool? Public { get; set; }
-        public string? Id { get; set; } = default!;
-        public string? Href { get; set; } = default!;
-        public string? SnapshotId { get; set; } = default!;
-        public List<Image>? Images { get; set; } = default!;
-
-        public Paging<PlaylistTrack<IPlayableItem>>? Tracks { get; set; } = default!;
-        public string? Type { get; set; } = default!;
-        public string? Uri { get; set; } = default!;
+        public bool? Collaborative { get; set; }
     }
 }
