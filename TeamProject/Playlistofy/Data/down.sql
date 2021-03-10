25dabbbff7179f2be0c78eeb087cb25dacc0dff1
ApneaMan29 <dmcbride15@wou.edu>
2021-03-10 09:12:50 -0800

    Updated down.sql to fit with the up.sql

--- TeamProject/Playlistofy/Data/down.sql
+++ TeamProject/Playlistofy/Data/down.sql
@@ -1,4 +1,12 @@
-﻿ALTER TABLE [Playlist] DROP CONSTRAINT [PK_Playlist];
+﻿ALTER TABLE [AspNetRoles] DROP CONSTRAINT [PK_AspNetRoles];
+ALTER TABLE [AspNetUsers] DROP CONSTRAINT [PK_AspNetUsers];
+ALTER TABLE [AspNetRoleClaims] DROP CONSTRAINT [PK_AspNetRoleClaims];
+ALTER TABLE [AspNetUserClaims] DROP CONSTRAINT [PK_AspNetUserClaims];
+ALTER TABLE [AspNetUserLogins] DROP CONSTRAINT [PK_AspNetUserLogins];
+ALTER TABLE [AspNetUserRoles] DROP CONSTRAINT [PK_AspNetUserRoles];
+ALTER TABLE [AspNetUserTokens] DROP CONSTRAINT [PK_AspNetUserTokens];
+ALTER TABLE [Playlist] DROP CONSTRAINT [PK_Playlist];
+ALTER TABLE [Track] DROP CONSTRAINT [PK_Track];
 
 
 DROP Table [Playlist];
