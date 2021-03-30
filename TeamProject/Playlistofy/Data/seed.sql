d55389c186839d6a34c4385fb8ca25c46567dccb
ApneaMan29 <dmcbride15@wou.edu>
2021-03-30 09:20:44 -0700

    Checking out Luis's db update

--- TeamProject/Playlistofy/Data/seed.sql
+++
@@ -1,5 +0,0 @@
-﻿INSERT INTO [User]([Id],[UserName],[Email],[EmailConfirmed],[PasswordHash],[PhoneNumber],[PhoneNumberConfirmed])
-        VALUES ('testuser','TestUser','testuser@test.com',1,'1234','503-541-6969',1);
-
-INSERT INTO [Playlist]([Id],[UserId],[Description],[Href],[Name],[Public],[Collaborative],[URI])
-        VALUES ('Playlist_1','testuser', 'This is my new playlist', 'This the HREF', 'My playlists Name', 1, 0, 'www.google.com');
