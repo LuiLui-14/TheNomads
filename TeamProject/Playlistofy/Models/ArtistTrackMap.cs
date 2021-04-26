2b76932703233a25048497576cde46c1ab271612
ApneaMan29 <dmcbride15@wou.edu>
2021-04-26 16:51:55 -0700

    Initial commit

--- TeamProject/Playlistofy/Models/ArtistTrackMap.cs
+++ TeamProject/Playlistofy/Models/ArtistTrackMap.cs
@@ -5,11 +5,7 @@ using System.Collections.Generic;
 
 namespace Playlistofy.Models
 {
-<<<<<<< HEAD
-    public partial class ArtistTrackMap
-=======
 public partial class ArtistTrackMap
->>>>>>> mcbride_artist_creation
     {
         public int Id { get; set; }
         public string ArtistId { get; set; }
